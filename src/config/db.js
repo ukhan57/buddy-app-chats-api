0e67a7be48b3b3fda86f0b150d46593975f32b21
ukhan57 <ukhan57@myseneca.ca>
2024-08-08 09:20:34 -0400

    Add logging for connection

--- src/config/db.js
+++ src/config/db.js
@@ -2,6 +2,7 @@
 
 // Importing mongoose to store messages
 const mongoose = require("mongoose");
+const logger = require("../logger");
 
 // Connect to MongoDB
 const connectDB = async () => {
@@ -9,9 +10,11 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
         });
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        logger.info(`MongoDB Connected: ${conn.connection.host}`)
     } catch (err) {
         console.log(`Error connecting MongoDB: ${err.message}`);
+        logger.error(`Error connecting MongoDB: ${err.message}`);
+        
         process.exit();
     }
 };
