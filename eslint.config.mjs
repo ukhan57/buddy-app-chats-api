147160228c6776b2fa78c411a66d8ca5459d084b
ukhan57 <ukhan57@myseneca.ca>
2024-08-08 20:16:04 -0400

    Fixing ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,17 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.node }},
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      ecmaVersion: 'latest',
+      globals: {
+        ...globals.node, // Use the node globals
+        // Add any other global variables you might need
+      },
+    },
+  },
   pluginJs.configs.recommended,
 ];
