6601a79dbc344f618a4b926f858415f07116ebfe
ukhan57 <ukhan57@myseneca.ca>
2024-08-09 04:28:37 -0400

    Add new route to fetch all the chats

---
+++ src/routes/api/fetchChats.js
@@ -0,0 +1,27 @@
+// src/roues/api/fetchChats.js
+
+const Chat = require("../../modals/chatModel");
+const User = require("../../modals/userModel");
+const logger = require('../../logger');
+
+module.exports = async (req, res) => {
+    try {
+        Chat.find({ users : { $elemMatch: { $eq: req.user._id } } })
+        .populate("users", "-password")
+        .populate("groupAdmin", "-password")
+        .populate("latestMessage")
+        .sort({ updatedAt: -1 })
+        .then(async (results) => {
+            results = await User.populate(results, {
+                path: "latestMessage.sender",
+                select: "name pic email",
+            });
+            logger.debug({ results }, 'Successuly fetch the chats');
+            res.status(200).send(results);
+        });
+    } catch (err) {
+        logger.error("Unable to fetch the chats")
+        res.status(400).json({ err: 'Unable to fetch the chat' });
+        throw new Error(err.message);
+    }
+};
