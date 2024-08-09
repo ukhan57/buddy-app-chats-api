8d2f215445cb35cd7646b5751fc70f701b1f5657
ukhan57 <ukhan57@myseneca.ca>
2024-08-08 21:31:28 -0400

    Updated the route to return all the users except for the logged-in user

--- src/routes/api/listUsers.js
+++ src/routes/api/listUsers.js
@@ -11,14 +11,43 @@ const logger = require('../../logger');
 module.exports = async (req, res) => {
   try {
     const params = {
-      UserPoolId: 'us-east-1_sTODudYpz',
+      UserPoolId: process.env.AWS_COGNITO_POOL_ID,
       Limit: 10, // Can be adjusted as needed, for now I am going with 10
     };
-    const users = await cognito.listUsers(params).promise();
-    res.json(users);
-    logger.info("Successfully retrieved the users", users);
+
+    // Decode the token to get the current user's username
+    const authHeader = req.headers.authorization;
+    const token = authHeader.split(" ")[1];
+    let decoded;
+
+    try {
+      decoded = JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
+      logger.info('Successfully Decoded the token');
+    } catch (error) {
+      logger.error('Failed to decode token: ', error);
+      return res.status(400).json({ error: "Invalid token" });
+    }
+
+    // Get the current username and password after decoding
+    const currentEmail = decoded.email;
+    const currentUser = decoded["cognito:username"];
+    logger.debug({ currentUser }, "Current user's username: ")
+    logger.debug({ currentEmail }, "Current user's email: ")
+    
+    // Fetch users from cognito
+    const users = await cognito.listUsers(params).promise(); // Fetch the users from Cognito
+    logger.info('Successfully Fetched the users from cognito');
+
+    // Filter out the currently authenticated/logged-in user
+    const filteredUsers = users.Users.filter(user => {
+      const emailAttr = user.Attributes.find(attr => attr.Name === 'email');
+      return emailAttr ? emailAttr.Value !== currentEmail : true;
+    });
+    logger.info('Successfully Filtered the Users');
+
+    res.json({ Users: filteredUsers });
+    logger.info("Successfully retrieved the users", filteredUsers);
   } catch (err) {
-    console.error("Error fetching users: ", err);
     logger.error("Failed to fetch users", err);
     res.status(500).json({ error: 'Failed to fetch users' });
   }
